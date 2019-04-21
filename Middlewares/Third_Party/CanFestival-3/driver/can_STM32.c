2adb1b9b5adfb6d1d6e44c0ba4215a9bdf1f51e9
zulolo <dodoleon@sina.com>
2019-04-21 22:37:37 +0800

    first commit

---
+++ Middlewares/Third_Party/CanFestival-3/driver/can_STM32.c
@@ -0,0 +1,42 @@
+/*
+This file is edited as a part of CanFestival.
+
+STM32F407 Port: Jim
+*/
+
+#include "stm32f4xx_hal.h"
+#include "can_STM32.h"
+#include "canfestival.h"
+#include "misc.h"
+
+/**
+ * Send a CAN Message Passed from the CANOpen Stack
+ * @param  CANx CAN_PORT used 1 or 2.
+ * @param  m    Pointer to Message to send
+ * @return      0-Failed, 1-Success
+ */
+unsigned char canSend(CAN_PORT CANx, Message *m)
+{
+	unsigned char i;
+	CAN_TxHeaderTypeDef   TxHeader;
+	uint8_t               TxData[8];
+	uint32_t              TxMailbox;
+	
+	TxHeader.StdId = (uint32_t)(m->cob_id);
+	TxHeader.ExtId = 0x00;
+	TxHeader.RTR = m->rtr;
+	TxHeader.IDE = CAN_ID_STD;
+	TxHeader.DLC = m->len; 
+	TxHeader.TransmitGlobalTime = DISABLE;	
+	for(i=0; i<m->len; i++) {
+		TxData[i] = m->data[i];
+	}
+	if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK) {
+		printf("CAN transmit error.\n");
+		return 0;
+	} else {
+		return 1;			
+	}     
+}
+
+/****************** (C) COPYRIGHT CSST Robot Research Center *****END OF FILE****/
---
+++ Src/canopen.c
@@ -0,0 +1,84 @@
+#include "stdio.h"
+#include "cmsis_os.h"
+
+#include "canfestival.h"
+#include "can_STM32.h"
+#include "CHASSIS_OD.h"
+
+/*store date CAN receive*/
+xQueueHandle xQ_CAN_MSG = NULL;
+xTaskHandle  xT_CANOpen = NULL;
+
+extern CO_Data *CO_CAN1;
+extern CAN_HandleTypeDef hcan1;
+
+void canopen_dataprocess_thread(void * pvParameters)
+{
+	int i;
+	unsigned char ret_canInit;
+	Message RxMSG = Message_Initializer; /* 标准的CAN2.0A信息，初始化清零 */
+	CANOpen_Message CAN_Rx_m;            /* CANOpen Message 包含CAN Port(CANx) */
+
+	/* CANOpen Initialising */
+	initTimer();
+	setNodeId (&CHASSIS_OD_Data, CHASSIS_ID);
+
+	CO_CAN1 = &CHASSIS_OD_Data;
+	CO_CAN1->canHandle = hcan1;  //Config CANOpen Port CAN1
+
+	/* State Machine Change to Initialisation and automatic go into Pre_operational*/
+	//setState(&ObjDict_CAN2_Data, Initialisation);
+	setState(&CHASSIS_OD_Data, Initialisation);
+	printf("State Machine change to Initialisation...\r\n");
+	/*****************************************************************************
+	* Initialisation -> Pre_operational -> StartOrStop ->
+	* startSYNC | heartbeatInit | emergencyInit | PDOInit | slaveSendBootUp
+	* startSYNC(CO_Data* d)       :监视OD-0x1005、0x1006，并根据其数值设定Alarm;
+	* heartbeatInit(CO_Data* d)   :监视OD-0x1017，consumers心跳，而后定Alarm;
+	* emergencyInit(CO_Data* d)   :监视OD-0x1003;
+	* PDOInit (CO_Data * d)       :监视OD-0x1800，初始化PDO;
+	* slaveSendBootUp(CO_Data* d) :发送BootUp指令。
+	*****************************************************************************/
+	/* State Machine change to Pro_operational */
+	printf("State Machine change to Pro_operational...\r\n");
+	printf("Slave Send a BootUp Message...\r\n");
+
+	/*create a queue can store 20 data*/
+	xQ_CAN_MSG = xQueueCreate(20, sizeof(CANOpen_Message));/* 20个CAN信息？ */
+
+	/* Success Or Fail */
+	if(NULL == xQ_CAN_MSG) {
+	/*failed to creat the queue*/
+		while(1) {	 
+			printf("!!!CANOpen Queue created failed!!!\r\n");
+			vTaskDelay(100);
+		}
+	} else {
+		printf("CANOpen Queue created successfully...\r\n");
+		printf("<<<<<CANOpen Thread ALL CLEAR>>>>>\r\n");
+	}
+
+	while(1) {
+	if(xQueueReceive(xQ_CAN_MSG, &(CAN_Rx_m), (portTickType)100)) {
+		printf("Thread get a CAN packege ");
+
+		/* 将队列中的数据存储到RxMSG中 */
+		RxMSG.cob_id = (uint16_t)(CAN_Rx_m.m.StdId);
+		RxMSG.rtr = CAN_Rx_m.m.RTR;
+		RxMSG.len = CAN_Rx_m.m.DLC;
+		for(i=0;i<RxMSG.len;i++) {
+			RxMSG.data[i] = CAN_Rx_m.m.Data[i]; //Transfer data[0-7] from CAN_Rx_m to RxMSG
+		}
+		printf("from CAN%u: 0x%x| ", CAN_Rx_m.CANx,CAN_Rx_m.m.StdId);
+		for(i=0;i<RxMSG.len;i++) {
+			printf("0x%x ", RxMSG.data[i]);
+		}
+		printf("\r\n");
+
+		/*Handle The Data Receive, 此处和对象字典进行交互*/
+		canDispatch(CO_CAN1, &RxMSG); 
+
+		vTaskDelay(CANOpen_THREAD_DELAY_TIMER);
+	}
+}
+
